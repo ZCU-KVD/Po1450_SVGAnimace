23c10ec36f8615317e55e5f4bab3f4cdd233e8e1
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2025-04-28 16:23:50 +0200

    Přidejte soubory projektu.

---
+++ Po1450_SVGAnimace/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Po1450_SVGAnimace;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRadzenComponents();
+
+await builder.Build().RunAsync();
